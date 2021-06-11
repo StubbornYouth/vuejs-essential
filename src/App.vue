956152fa5361fe53a38c223471f6bc13649213b2
StubbornYouth <757336260@qq.com>
2021-06-11 09:53:09 +0800

    add github pages

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- 通过为 template 和 style 指定 lang 属性，我们就可以使用其他语法来书写代码 
+  npm install pug-plain-loader pug sass-loader node-sass - -save-dev
+  pug 是一款模板引擎，sass-loader 和 node-sass 用来处理 Sass（一种 CSS 扩展语言），- -save-dev 用来标识当前模块是开发所需的模块
+-->
+
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 200px;
+
+  h1 {
+    color: #42b983;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// 引入 Vue.js
 import Vue from 'vue'
+// 引入 入口文件
 import App from './App.vue'
 
+// 生产提示
 Vue.config.productionTip = false
 
+// 创建一个新的Vue实例
 new Vue({
   render: h => h(App),
 }).$mount('#app')
